3a61a91e9598c3f43bd8c0dd69cdbbd22a8d719e
micael-ricardo <micael.ricardo@outlook.com>
2023-04-30 19:49:48 -0300

    Ajustes

---
+++ resources/views/template/header.blade.php
@@ -0,0 +1,20 @@
+<nav class="navbar bg-dark" data-bs-theme="dark">
+    <div class="container-fluid">
+        <a class="navbar-brand" href="{{ route('adm.dashboard') }}">
+            <i class="bi bi-speedometer"></i> Dashboard
+        </a>
+        <ul class="navbar-nav ms-auto">
+            <li class="nav-item text-light">
+                Bem vindo, {{ Auth::user()->nome }}!
+            </li>
+        </ul>
+        <ul class="navbar-nav ms-auto">
+            <li class="nav-item">
+                <form action="/logout" method="POST">  
+                    @csrf
+                    {{-- <a href="{{ route('logout') }}" class="nav-link">Sair  <i class="bi bi-box-arrow-right"></i></a> --}}
+                </form>
+            </li>
+        </ul>
+    </div>
+</nav>
---
+++ resources/views/template/sidebar.blade.php
@@ -0,0 +1,21 @@
+<div class="container-fluid">
+    <div class="row flex-nowrap">
+        <div class="col-auto col-md-2 col-xl-2 px-sm-2 px-0">
+            <div class="d-flex flex-column align-items-center align-items-sm-start px-3 pt-2 text-white min-vh-100" data-bs-theme="dark">
+                <ul class="nav nav-pills flex-column mb-sm-auto mb-0 align-items-center align-items-sm-start text-white" 
+                    id="menu">
+                    @auth
+                        <li>
+                            <a href="#" class="nav-link px-0 align-middle">
+                                <i class="fs-4 bi-coin"></i><span class="ms-1  d-sm-inline">Vagas</span></a>
+                        </li>
+                        <li>
+                            <a href="#" class="nav-link px-0 align-middle">
+                                <i class="fs-4 bi-people"></i> <span class="ms-1 d-none d-sm-inline">Usuarios</span>
+                            </a>
+                        </li>
+                    @endauth
+                </ul>
+                <hr>
+            </div>
+        </div>
