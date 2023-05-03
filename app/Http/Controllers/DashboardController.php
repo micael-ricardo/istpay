7369bd0b5026fa900838c83ba08ecf8f011db49f
micael-ricardo <micael.ricardo@outlook.com>
2023-05-03 10:18:19 -0300

    criar model para listar a view

--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -3,10 +3,15 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\vagas_candidato_view;
+
 
 class DashboardController extends Controller
 {
   public function index(){
+
+//     $query = vagas_candidato_view::all();
+// dd( $query );
 
     return view('adm.dashboard');
   }
